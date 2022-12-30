0f621dd57a057b732a1c0ead7f7fa6da6bddd36d
jktang14 <97177453+jktang14@users.noreply.github.com>
2022-12-30 22:07:07 +0800

    Create function that runs a game

--- script.js
+++ script.js
@@ -9,26 +9,33 @@ function getComputerChoice() {
 
 // Play round and declare winner
 function playRound(playerSelection, computerSelection) {
-    let allLower = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-    if (allLower === computerSelection) {
+    if (playerSelection === computerSelection) {
         return `Its a tie!\nUser score: ${userScore}\nComputer score: ${computerScore}`;
     }
-    else if (allLower === "Rock" && computerSelection === "Scissors") {
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
         return `You win! Rock beats scissors\nUser score: ${userScore += 1}\nComputer score: ${computerScore}`;
     }
-    else if (allLower === "Rock" && computerSelection === "Paper") {
+    else if (playerSelection === "Rock" && computerSelection === "Paper") {
         return `You lose! Paper beats rock\nUser score: ${userScore}\nComputer score: ${computerScore += 1}`;    
     }
-    else if (allLower === "Scissors" && computerSelection === "Rock") {
+    else if (playerSelection === "Scissors" && computerSelection === "Rock") {
         return `You lose! Rock beats scissors\nUser score: ${userScore}\nComputer score: ${computerScore += 1}`;
     }
-    else if (allLower === "Scissors" && computerSelection === "Paper") {
+    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
         return `You win! Scissors beats paper"\nUser score: ${userScore += 1}\nComputer score: ${computerScore}`;
     }
-    else if (allLower === "Paper" && computerSelection === "Scissors") {
+    else if (playerSelection === "Paper" && computerSelection === "Scissors") {
         return `You lose! Scissors beats paper\nUser score: ${userScore}\nComputer score: ${computerScore += 1}`;
     }
-    else if (allLower === "Paper" && computerSelection === "Rock") {
+    else if (playerSelection === "Paper" && computerSelection === "Rock") {
         return `You win! Paper beats rock\nUser score: ${userScore += 1}\nComputer score: ${computerScore}`;
+    }
+}
+
+function game() {
+    for (i = 0; i < 5; i++) {
+        let userAnswer = prompt("Choose your attack! Scissors, Paper or Rock");
+        let allLower = userAnswer[0].toUpperCase() + userAnswer.slice(1).toLowerCase();
+        console.log(playRound(allLower, getComputerChoice()));
     }
 }
