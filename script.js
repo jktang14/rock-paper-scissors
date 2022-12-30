6a76ae25ee59ef76d64a5ca0b4110f5040d7e06e
jktang14 <97177453+jktang14@users.noreply.github.com>
2022-12-30 21:38:40 +0800

    Add score functionality to playRound

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let choices = ['Rock', 'Paper', 'Scissors']
+let choices = ['Rock', 'Paper', 'Scissors'];
+let userScore = 0;
+let computerScore = 0;
 // Return rock,paper,scissors randomly
 function getComputerChoice() {
     let pose = Math.floor(Math.random() * 3);
@@ -9,24 +11,24 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     let allLower = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
     if (allLower === computerSelection) {
-        return "Its a tie!";
+        return `Its a tie!\nUser score: ${userScore}\nComputer score: ${computerScore}`;
     }
     else if (allLower === "Rock" && computerSelection === "Scissors") {
-        return "You win! Rock beats scissors";
+        return `You win! Rock beats scissors\nUser score: ${userScore += 1}\nComputer score: ${computerScore}`;
     }
     else if (allLower === "Rock" && computerSelection === "Paper") {
-        return "You lose! Paper beats rock";    
+        return `You lose! Paper beats rock\nUser score: ${userScore}\nComputer score: ${computerScore += 1}`;    
     }
     else if (allLower === "Scissors" && computerSelection === "Rock") {
-        return "You lose! Rock beats scissors";
+        return `You lose! Rock beats scissors\nUser score: ${userScore}\nComputer score: ${computerScore += 1}`;
     }
     else if (allLower === "Scissors" && computerSelection === "Paper") {
-        return "You win! Scissors beats paper";
+        return `You win! Scissors beats paper"\nUser score: ${userScore += 1}\nComputer score: ${computerScore}`;
     }
     else if (allLower === "Paper" && computerSelection === "Scissors") {
-        return "You lose! Scissors beats paper";
+        return `You lose! Scissors beats paper\nUser score: ${userScore}\nComputer score: ${computerScore += 1}`;
     }
     else if (allLower === "Paper" && computerSelection === "Rock") {
-        return "You win! Paper beats rock";
+        return `You win! Paper beats rock\nUser score: ${userScore += 1}\nComputer score: ${computerScore}`;
     }
 }
